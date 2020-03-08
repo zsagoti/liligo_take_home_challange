0a732657aec431d7e2922bcdf007279b2b3da06a
Agota Zsolczer <zsolczer.agota@gmail.com>
2020-03-08 14:24:05 +0100

    First insights

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 def load_listings(listings_urls):
@@ -15,5 +16,20 @@ def load_listings(listings_urls):
             print('Done.')
         else:
             print(location + " data has already downloaded.")
-        listings_data[location] = pd.read_csv(raw_file)
+        listings_data[location] = pd.read_csv(raw_file, dtype={'neighbourhood':'str',
+                                                               'zipcode':'str',
+                                                               'weekly_price':'str',
+                                                               'monthly_price':'str',
+                                                               'license':'str'})
     return pd.concat(listings_data,names=['location']).droplevel(1)
+    
+def my_plot_pie(data, title='',suptitle=''):
+    labels = data.index
+    sizes = data.values
+    explode = len(labels)*[0.01]
+    plt.pie(sizes, explode=explode, labels=labels,# colors=colors,
+    autopct='%1.1f%%', shadow=False, startangle=90)
+    plt.title(title)
+    plt.suptitle(suptitle)                 
+    plt.axis('equal')
+    plt.show()
